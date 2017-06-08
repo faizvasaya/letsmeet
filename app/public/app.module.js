bd288f15b4fbe5eaa0ebe0db5f52577a5b81de43
Faizal Vasaya <faizvasya@gmail.com>
2017-06-08 07:49:31 +0530

    Add app.module.js instead of app.js

---
+++ app/public/app.module.js
@@ -0,0 +1,8 @@
+/**
+ * @author Faizal Vasaya
+ * @date 6/8/2017
+ * @desc Angular module to initialize the application.
+ */
+(function () {
+    var app = angular.module("app", ["ngRoute"]);
+})();
