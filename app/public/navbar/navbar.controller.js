a3b79d7a33e6a349676e1ffed0abc07df6b95fbd
Faizal Vasaya <faizvasya@gmail.com>
2017-06-08 10:01:46 +0530

    Add link for login

--- app/public/navbar/navbar.controller.js
+++ app/public/navbar/navbar.controller.js
@@ -27,7 +27,7 @@
                 name: 'Blogs'
             },
             {
-                link: '#',
+                link: '#!login',
                 name: 'Login'
             },
             {
