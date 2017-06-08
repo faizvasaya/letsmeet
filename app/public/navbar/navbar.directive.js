3a187da6c33b678cfe08754e6ef258b307601fd1
Faizal Vasaya <faizvasya@gmail.com>
2017-06-08 07:51:17 +0530

    Add comments and wrapped IIFE to the directive

--- app/public/navbar/navbar.directive.js
+++ app/public/navbar/navbar.directive.js
@@ -2,14 +2,20 @@
  * @author Faizal Vasaya
  * @date 8/6/2017
  * @lastModified 8/6/2017
- * @directive A directive to display navbar. It loads navbar.html from the current
+ * @desc A directive to display navbar. It loads navbar.html from the current
  *            folder. The node server points to public hence the path should be 
  *            from the same folder.
+ * @example <navbar>
  */
-var app = angular.module('app');
+//Wrapped inside an IIFE to avoid global namespace pollution.
+(function () {
 
-app.directive('navbar', function(){
-    return{
-        templateUrl: 'navbar/navbar.html'
-    }
-});
+    var app = angular.module('app');
+
+    app.directive('navbar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'navbar/navbar.html'
+        }
+    });
+})();
