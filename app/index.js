c38a3061c1b277e090e1d5cc85f947106d648987
Faizal Vasaya <faizvasya@gmail.com>
2017-06-08 07:48:46 +0530

    Remove basic express routing

--- app/index.js
+++ app/index.js
@@ -2,8 +2,4 @@ var express =  require('express');
 var path = require('path');
 var app =  express();
 app.use(express.static(path.join(__dirname,'public')));
-app.get('/', function(req, resp){
-    resp.send('Hello World');
-});
-
 app.listen(3000);
