69894af194f7f761417dde4049c60fd3f71190ac
Faizal Vasaya <faizvasya@gmail.com>
2017-06-08 15:09:19 +0530

    Basic controller set up for login controller

--- app/public/login/login.controller.js
+++ app/public/login/login.controller.js
@@ -3,11 +3,26 @@
 
     app.controller('LoginController', LoginController);
 
-    LoginController.$inject = ['$scope'];
+    LoginController.$inject = ['$scope', '$location', 'authenticationService'];
 
-    function LoginController($scope) {
+    function LoginController($scope, $location, authenticationService) {
         var vm = this;
         vm.username = '';
         vm.password = '';
+
+        vm.redirect = redirect;
+        vm.authenticateUsingGoogle = authenticateUsingGoogle;
+        vm.authenticateUsingFacebook = authenticateUsingFacebook;
+
+        function redirect() {
+            $location.path('/homepage');
+        }
+
+        function authenticateUsingGoogle() {
+            authenticationService.google();
+        }
+        function authenticateUsingFacebook() {
+            authenticationService.facebook();
+        }
     }
 })();
