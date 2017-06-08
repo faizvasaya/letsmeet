057d885d0570e4d4908e1a713cabce5efc5caf0b
Faizal Vasaya <faizvasya@gmail.com>
2017-06-08 09:42:00 +0530

    Create a controller for Navbar

---
+++ app/public/navbar/navbar.controller.js
@@ -0,0 +1,39 @@
+/**
+ * @author Faizal Vasaya
+ * @date 6/8/2017
+ * @desc A controller to dynamically change menu items of the navbar. Controller As
+ *       syntax has been used to initialize the controller in the HTML.
+ */
+(function () {
+    var app = angular.module('app');
+
+    app.controller('NavbarController', NavbarController);
+
+    NavbarController.$inject = ['$scope'];
+
+    function NavbarController($scope) {
+        var vm = this;
+        vm.menu = [
+            {
+                link: '#',
+                name: 'Home'
+            },
+            {
+                link: '#',
+                name: 'Browse Meetups'
+            },
+            {
+                link: '#',
+                name: 'Blogs'
+            },
+            {
+                link: '#',
+                name: 'Login'
+            },
+            {
+                link: '#!contactus',
+                name: 'Contactus'
+            }
+        ];
+    }
+})();
