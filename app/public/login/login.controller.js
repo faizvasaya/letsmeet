ca983343e4ffb1d71e85cf42eb7bf3879628c23b
Faizal Vasaya <faizvasya@gmail.com>
2017-06-08 10:01:09 +0530

    Create a basic login controller

---
+++ app/public/login/login.controller.js
@@ -0,0 +1,11 @@
+(function () {
+    var app = angular.module('app');
+
+    app.controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$scope'];
+
+    function LoginController($scope) {
+
+    }
+})();
