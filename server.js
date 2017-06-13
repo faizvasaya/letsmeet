49bb61cf77239ccf171c887036cd961654bd501f
Faizal Vasaya <faizvasya@gmail.com>
2017-06-13 13:09:51 +0530

    Create server and added mongoose support

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var path = require('path');
 var app = express();
 var port = process.env.PORT || 3000;
+var router = express.Router();
+var appRoutes = require('./app/routes/signup.api')(router);
 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+app.use('/api', appRoutes);
+
+mongoose.connect('mongodb://localhost:27017/letsmeet', function (err) {
+    if (err) {
+        console.log('Not connected to the database: ' + err);
+    } else {
+        console.log('Successfully connected');
+    }
+});
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/app/index.html'));
