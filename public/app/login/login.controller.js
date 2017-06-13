7733a3fbbb8bdfd2f8dcdc7dc5341e6179ac5efb
Faizal Vasaya <faizvasya@gmail.com>
2017-06-13 20:25:22 +0530

    Final login controller

--- public/app/login/login.controller.js
+++ public/app/login/login.controller.js
@@ -7,15 +7,25 @@
 
     function LoginController($scope, $location, authenticationService) {
         var vm = this;
-        vm.username = '';
-        vm.password = '';
 
         vm.redirect = redirect;
+        vm.authenticate = authenticate;
         vm.authenticateUsingGoogle = authenticateUsingGoogle;
         vm.authenticateUsingFacebook = authenticateUsingFacebook;
 
         function redirect() {
             $location.path('/homepage');
+        }
+
+        function authenticate(){
+            var authPromise = authenticationService.authenticate(vm.data);
+            authPromise.then(function(response) {
+                 vm.message= response.data.message;
+                 vm.status = response.data.success;
+            }, function(error) {
+                vm.message= response.data.message;
+                vm.status = response.data.success;
+            });
         }
 
         function authenticateUsingGoogle() {
