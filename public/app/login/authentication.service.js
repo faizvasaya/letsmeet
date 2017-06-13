a1b38fcf474b11661ca25ceb803db2b0210d8b05
Faizal Vasaya <faizvasya@gmail.com>
2017-06-13 20:24:57 +0530

    Create auth service

--- public/app/login/authentication.service.js
+++ public/app/login/authentication.service.js
@@ -2,14 +2,19 @@
     var app = angular.module('app');
 
     app.factory('authenticationService', authenticationService);
+    authenticationService.$inject = ['$http'];
 
-    function authenticationService() {
+    function authenticationService($http) {
         var authenticationService = {
+            authenticate: authenticate,
             google: googleAuthentication,
             facebook: facebookAuthentication
         };
         return authenticationService;
 
+        function authenticate(data) {
+            return $http.post('/api/auth', data);
+        }
         function googleAuthentication() {
             
         }
