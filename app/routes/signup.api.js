1423540efd7b63415fe8881e4ea5c2b4e5b045a7
Faizal Vasaya <faizvasya@gmail.com>
2017-06-13 20:24:16 +0530

    Include auth in signup

--- app/routes/signup.api.js
+++ app/routes/signup.api.js
@@ -27,5 +27,40 @@ module.exports = function (router) {
             });
         }
     });
+
+    router.post('/auth', function (req, res) {
+       User.findOne({
+           email: req.body.email
+        }).select('email password name')
+          .exec(function (err, user){
+        if(err) throw err;
+        if(!user){
+            res.json({
+                success: false,
+                message: 'Unregistered Email'
+            });
+        } else if(user) {
+            if(req.body.password) {
+                if(!user.comparePassword(req.body.password)){
+                    res.json({
+                        success: false,
+                        message: 'Invalid Password'
+                    });
+                } else {
+                    res.json({
+                           success: true,
+                           message: 'Successful Login'
+                    });
+                }
+            } else {
+                 res.json({
+                            success: false,
+                            message: 'No password provided'
+                });
+            }
+        }
+    }) 
+    });
+
     return router;
 }
